77a0fd84a182af5ff67b5bbc7f06f57060e00977
Gravit Gravit <gravit.min@ya.ru>
2017-01-27 17:21:14 +0700

    Fix build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ fpc code.pas
 rm code.o
 rm linux
 mv code linux
-echo THE END
-nano link.txt
 wine ~/.wine/drive_c/FPC/3.0.0/bin/i386-win32/fpc.exe Z:/home/gravit/code/pascalprojects/$dir/code.pas
+rm code.o
+echo THE END
