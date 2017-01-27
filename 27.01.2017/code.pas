77506e20c592bc00a49dbf6493677e097e67f8f9
Gravit Gravit <gravit.min@ya.ru>
2017-01-27 15:44:11 +0700

    Fix 27.01.2017

--- 27.01.2017/code.pas
+++ 27.01.2017/code.pas
@@ -3,6 +3,6 @@ var price,discountprice : real;
 begin
         write('Please print price: ');
         readln(price);
-        discountprice:=(price*(12/100))+price; //Discount Price = price + ( 12% from price )
+        discountprice:=price-(price*(12/100)); //Discount Price = price - ( 12% from price )
         writeln('discountprice = ',discountprice : 10 : 2)
 end.
